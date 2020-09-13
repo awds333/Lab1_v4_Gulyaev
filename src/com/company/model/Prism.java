af932da9553913dfbdf18f07fa5b6cc55fdb86ba
Fedchanka Yura <wn8yura@gmail.com>
2020-09-13 22:36:21 +0300

    WIP

---
+++ src/com/company/model/Prism.java
@@ -0,0 +1,11 @@
+package com.company.model;
+
+public class Prism {
+    private Square base;
+    private Section height;
+
+    Prism(Square base, Section height) {
+        this.base = base;
+        this.height = height;
+    }
+}
---
+++ src/com/company/model/Pyramid.java
@@ -0,0 +1,11 @@
+package com.company.model;
+
+public class Pyramid {
+    private Square base;
+    private Section height;
+
+    Pyramid(Square base, Section height) {
+        this.base = base;
+        this.height = height;
+    }
+}
